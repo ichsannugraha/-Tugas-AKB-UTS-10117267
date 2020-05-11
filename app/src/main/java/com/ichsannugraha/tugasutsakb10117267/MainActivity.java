7b98e852c4a58d5fc637c5abd7c3516c28e82a7a
Ichsan N <nugraha.ichsan99@gmail.com>
2020-05-11 15:37:24 +0700

    Tugas UTS AKB 10117267

--- app/src/main/java/com/ichsannugraha/tugasutsakb10117267/MainActivity.java
+++ app/src/main/java/com/ichsannugraha/tugasutsakb10117267/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment()).commit();
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-    }
+    }fesa dexz feas
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
