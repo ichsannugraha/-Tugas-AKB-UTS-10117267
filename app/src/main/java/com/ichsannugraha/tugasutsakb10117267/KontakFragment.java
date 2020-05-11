86c485233554f97d452734948cefb3cbee06c261
Ichsan N <nugraha.ichsan99@gmail.com>
2020-05-11 14:42:29 +0700

    Tugas UTS AKB 10117267

---
+++ app/src/main/java/com/ichsannugraha/tugasutsakb10117267/KontakFragment.java
@@ -0,0 +1,23 @@
+package com.ichsannugraha.tugasutsakb10117267;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class KontakFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_kontak, container, false);
+
+
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/ichsannugraha/tugasutsakb10117267/MainActivity.java
@@ -0,0 +1,49 @@
+package com.ichsannugraha.tugasutsakb10117267;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setSelectedItemId(R.id.nav_profile);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProfileFragment()).commit();
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_kontak:
+                            selectedFragment = new KontakFragment();
+                            break;
+                        case R.id.nav_teman:
+                            selectedFragment = new TemanFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
